0bf9694a325e963f5717c126b24a318e8061e1b3
michal631235 <michal631235s@gmail.com>
2022-05-24 00:31:34 +0200

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import Task from './components/Task';
+import { useState , useEffect } from "react";
 
 function App() {
+
+    const [ isLoading, setLoading] = useState(true)
+    const [ loadedData, setData] = useState([])
+
+    useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        let changedData = [];
+        for(let i = 0; i<10; i++)
+        {
+          changedData.push(data[i]) 
+        }
+          setLoading(false)
+          setData(changedData)
+      })
+    }, [isLoading])
+
+    if (isLoading) {
+      return (
+        <section>
+          <p>Loading...</p>
+        </section>
+      );
+    }
+
   return (
     <div>
       <h1 className="header">ToDoList</h1>
       <div className="tasks">
-        <Task title="zadanie1"/>
-        <Task title="zadanie2"/>
-        <Task title="zadanie3"/>
+      {loadedData.map(({title, completed}) => (
+        <Task title={title} completed={completed}/>
+      ))}
       </div>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
-function Task({title}) {
+function Task({title, completed}) {
     return(
         <div className="task">
             <div><h2>{title}</h2></div>
             <div>
                 <p>treść zadania</p>
                 <div className="bottomTask">
-                    <div>data</div><div>stan</div>
+                    <div>{completed ? "wykonane" : "niewykonane"}</div>
                 </div>
                 <div>
                     <button className="btn">Edytuj</button><button className="btn">Wykonane</button>
